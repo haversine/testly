af5e8a4cf29b00dbc447274e2786c92a69fb3234
Madis <madis.kase.001@khk.ee>
2013-04-15 12:53:59 +0300

    started work on MVC

---
+++ classes/Request.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: madis.kase.001
+ * Date: 15.04.13
+ * Time: 12:48
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Request {
+
+	public $controller = DEFAULT_CONTROLLER;
+	public $actions = 'index';
+	public $params = array();
+
+
+}
--- index.php
+++ index.php
@@ -23,7 +23,9 @@
 <![endif]-->
 
 <!-- Add your site or application content here -->
-<p>Hello world! This is HTML5 Boilerplate.</p>
+<?php require $_GET['page'].'.php'; ?>
+
+
 
 
 
